51ced224cca9e5e1153c7cfe6df99ee256fc83d6
maximoappendino <maximoappendinopadilla@gmail.com>
2025-02-19 16:31:00 -0300

    Base OpenGL, GLFW, GLEW. Set up. Right now it's just a dark teal screen.

---
+++ build/CMakeFiles/glfw_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glfw_project.dir/link.d"
+  "CMakeFiles/glfw_project.dir/src/main.cpp.o"
+  "CMakeFiles/glfw_project.dir/src/main.cpp.o.d"
+  "glfw_project"
+  "glfw_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glfw_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
